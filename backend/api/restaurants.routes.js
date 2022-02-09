7b6cd51fc8643f61eeb5f26f85fbacf15555965f
Kebron Solomon Z <keb2@MacBook-Pro.local>
2022-02-09 21:06:43 +0100

    add review controller

--- backend/api/restaurants.routes.js
+++ backend/api/restaurants.routes.js
@@ -1,7 +1,14 @@
 import express, { Router } from "express";
 import RestaurantsCtr from './restaurants.controller.js'
-const router = express.Router()
+import ReviewCtr from 'review.controller.js'
 
+const router = express.Router()
 router.route('/').get(RestaurantsCtr.apiGetRestaurants)
+
+router
+.route('/Review')
+.post(ReviewCtr.apiPostReview)
+.put(ReviewCtr.apiUpdateReview)
+.delete(ReviewCtr.apiDeleteReview)
 
 export default router
